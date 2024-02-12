bb87a5d45afb8d8ec9c2fd00b657cf4a3cb76de5
Ирина Мартоян <surenira@icloud.com>
2024-02-12 12:25:29 +0400

    Pagination polymorph component

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
 import { LeftArrowButton } from '@/components/ui/pagination/LeftArrowButton'
 import { PaginationButtons } from '@/components/ui/pagination/PaginationButtons'
 import { RightArrowButton } from '@/components/ui/pagination/RightArrowButton'
@@ -7,17 +9,20 @@ import s from './pagination.module.scss'
 
 import { usePagination } from './usePagination'
 
-type PaginationType = {
+type PaginationType<T extends ElementType> = {
+  as?: T
   className?: string
   currentPage: number
   onPageChange: (page: number) => void
   pageSize: number
   siblingCount: number
   totalCount: number
-}
+} & ComponentPropsWithoutRef<T>
 
-export const Pagination = (props: PaginationType) => {
-  const { currentPage, onPageChange, pageSize, siblingCount = 1, totalCount } = props
+export const Pagination = <T extends ElementType = 'a'>(
+  props: PaginationType<T> & Omit<ComponentPropsWithoutRef<T>, keyof PaginationType<T>>
+) => {
+  const { as, currentPage, onPageChange, pageSize, siblingCount = 1, totalCount, ...rest } = props
 
   const { isFirstPage, isLastPage, onNext, onPrevious, paginationRange } = usePagination({
     currentPage,
@@ -41,6 +46,7 @@ export const Pagination = (props: PaginationType) => {
           // onClick={handlePageClicked}
           onClick={onPageChange}
           paginationRange={paginationRange}
+          {...rest}
         />
 
         <RightArrowButton disabled={isLastPage} onClick={onNext} />
--- src/components/ui/pagination/PaginationButtons.tsx
+++ src/components/ui/pagination/PaginationButtons.tsx
@@ -1,15 +1,16 @@
-import { FC } from 'react'
+import { ComponentPropsWithoutRef, ElementType, FC } from 'react'
 
 import { clsx } from 'clsx'
 
 import s from './pagination.module.scss'
 
-type PaginationButtonsType = {
+type PaginationButtonsType<T extends ElementType> = {
+  as?: T
   currentPage: number
   disabled?: boolean
   onClick: (pageNumber: number) => void
   paginationRange: (number | string)[]
-}
+} & ComponentPropsWithoutRef<T>
 
 const classNames = {
   dots: s.dots,
@@ -19,53 +20,35 @@ const classNames = {
   },
 }
 
-export const PaginationButtons: FC<PaginationButtonsType> = ({
-  currentPage,
-  onClick,
-  paginationRange,
-}) => {
+export const PaginationButtons = <T extends ElementType = 'a'>(
+  props: PaginationButtonsType<T> &
+    Omit<ComponentPropsWithoutRef<T>, keyof PaginationButtonsType<T>>
+) => {
+  const { as: Component = 'a', currentPage, onClick, paginationRange, ...rest } = props
+
   return (
     <>
       {paginationRange.map((page: number | string, index) => {
         const isSelected = page === currentPage
 
-        // If the pageItem is a DOT, render the DOTS unicode character
         if (typeof page !== 'number') {
           return <Dots key={index} />
         }
 
-        // Render our Page Pills
         return (
-          <div
+          <Component
             className={classNames.pageButton(isSelected)}
             key={index}
             onClick={() => onClick(page)}
+            {...rest}
           >
             {page}
-          </div>
+          </Component>
         )
       })}
     </>
   )
 }
-
-// type PageButtonProps = {
-//   disabled?: boolean
-//   onClick: () => void
-//   page: number
-//   selected: boolean
-// }
-// const PageButton: FC<PageButtonProps> = ({ disabled, onClick, page, selected }) => {
-//   return (
-//     <button
-//       className={classNames.pageButton(selected)}
-//       disabled={selected || disabled}
-//       onClick={onClick}
-//     >
-//       {page}
-//     </button>
-//   )
-// }
 
 const Dots: FC = () => {
   return <span className={s.dots}>&#8230;</span>
